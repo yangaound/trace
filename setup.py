a36182e9d70f11d60a58b9a60768dbeaccecc230
yangaound <yangaound@users.noreply.github.com>
2017-10-27 23:58:49 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = '1.0.0'
+
+package_dir = 'trace'
+
+setup(name='trace',
+
+    version=version,
+
+    description='trace python program context',
+
+    packages=[package_dir, ],
+)
